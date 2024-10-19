8ce88ca9c834f8ebb5c4ac083702a27f8f7c1e79
linckrafael <rafaelinck@hotmail.com>
2024-10-19 03:50:01 -0300

    First commit

---
+++ javascript.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+AOS.init();
+
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
